aacdd538ebdfdd4cb4639a0f1a6492efa31c1f15
Specialist17 <arocho.fernando.13@gmail.com>
2017-08-11 18:47:11 -0700

    pagination done, working on activity indicator

---
+++ KnowYourDestination/UIActivityIndicator+Theme.swift
@@ -0,0 +1,9 @@
+//
+//  UIActivityIndicator+Theme.swift
+//  KnowYourDestination
+//
+//  Created by Fernando on 8/11/17.
+//  Copyright © 2017 Specialist. All rights reserved.
+//
+
+import Foundation
